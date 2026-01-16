5b0167a00f591e3524fa4c1d3493998b5cd9c18a
HA <myhaclassb1@gmail.com>
2026-01-16 13:52:20 +0700

    chore(deploy): setup deploy render and vercel config

--- backend/src/config/vnpayConfig.js
+++ backend/src/config/vnpayConfig.js
@@ -2,8 +2,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const vnpayConfig = {
-  vnp_TmnCode: process.env.VNP_TMN_CODE || "YOUR_TMN_CODE",
-  vnp_HashSecret: process.env.VNP_HASH_SECRET || "YOUR_HASH_SECRET",
+  vnp_TmnCode: process.env.VNP_TMN_CODE ,
+  vnp_HashSecret: process.env.VNP_HASH_SECRET ,
   vnp_Url:
     process.env.VNP_URL || "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
   vnp_ReturnUrl:
